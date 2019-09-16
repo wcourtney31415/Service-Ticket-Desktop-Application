a36bf02651500c2e18abf0d4ebf4431ae939a305
wcourtney31415 <wcourtney31415@gmail.com>
2019-09-16 19:25:04 -0400

    Added a button to perform an http request and populate the listbox with clients. This was mostly added as a proof of concept starting point. Also added a Client class for deserialization of JSON strings. For the deserializing, I added JSON.Net package from Nuget.

---
+++ Service Ticket Desktop Application/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service_Ticket_Desktop_Application
+{
+    class Client
+    {
+        public String firstName, lastName, phoneNumber;
+    }
+}
--- Service Ticket Desktop Application/Form1.cs
+++ Service Ticket Desktop Application/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,27 @@ namespace Service_Ticket_Desktop_Application
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            WebRequest webRequest = WebRequest.Create("http://localhost/client");
+
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            webRequest.Method = "GET";
+            webRequest.ContentType = "application/json";
+           
+            WebResponse webResp = webRequest.GetResponse();
+
+            Stream objWebResp = webResp.GetResponseStream();
+            StreamReader sr = new StreamReader(objWebResp);
+            String json = sr.ReadLine();
+            Console.WriteLine(json);
+            List<Client> clients = JsonConvert.DeserializeObject<List<Client>>(json);
+            foreach(Client client in clients)
+            {
+                listBox1.Items.Add(client.lastName + ", " + client.firstName + ": " + client.phoneNumber);
+            }
         }
     }
 }
